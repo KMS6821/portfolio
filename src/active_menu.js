f9663028bfb4ef2f1cfac789728d82f636f4d5d1
KMS6821 <98877660+KMS6821@users.noreply.github.com>
2023-09-25 19:47:37 +0900

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -11,18 +11,23 @@ const sectionIds = [
   "#about",
   "#skills",
   "#work",
-  "testimonial",
+  "#testimonial",
   "#contact",
 ];
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
-const visibleSections = sectionIds.map(() => false);
 
-const options = {};
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
-sections.forEach((section) => observer.observe);
+sections.forEach((section) => observer.observe(section));
 
 function observerCallback(entries) {
   let selectLastOne; // flag 변수 : true인지 아닌지를 간직하는 변수
@@ -32,15 +37,24 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
-    : findFirstInersecting(visibleSections);
+    : findFirstIntersecting(visibleSections);
+  selectNavItem(navIndex);
 }
 
-function findFirstInersecting(intersections) {
+function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
